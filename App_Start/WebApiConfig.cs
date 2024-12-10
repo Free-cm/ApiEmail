43bc092d7a4eaf322fcaac35bc5fdbc8de0a5cea
Free-cm <contactofreelanceryr@gmail.com>
2024-12-10 13:30:46 -0500

    Primer Commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SendEmail
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Habilitar CORS
+            var cors = new EnableCorsAttribute("*", "*", "*"); // Permite todas las fuentes, encabezados y métodos
+            config.EnableCors(cors);
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/SendEmailController.cs
@@ -0,0 +1,50 @@
+﻿using SendEmail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SendEmail.Controllers
+{
+    public class SendEmailController : ApiController
+    {
+        // POST api/example
+        [HttpPost]
+        public async Task<IHttpActionResult> Post(Email email)
+        {
+            try
+            {
+                // Configuración del cliente SMTP
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587, // Cambia el puerto según tu servidor SMTP
+                    Credentials = new NetworkCredential("contactofreelanceryr@gmail.com", "zvlh dfhg lnvc lywi"),
+                    EnableSsl = true
+                };
+
+                // Configuración del mensaje de correo
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("yourEmail@example.com"),
+                    Subject = email.Subject,
+                    Body = email.Body,
+                    IsBodyHtml = true
+                };
+                mailMessage.To.Add(email.To);
+
+                // Envío del correo
+                await smtpClient.SendMailAsync(mailMessage);
+
+                return Ok(email);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
